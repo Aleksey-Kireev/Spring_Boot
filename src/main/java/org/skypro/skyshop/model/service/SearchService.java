7adb98602dac66365504d50e37337359c04ca12d
Aleksey-Kireev <kireev_a_a@mail.ru>
2025-07-20 05:07:30 +0900

    DZ - 9 - Spring Boot На проверку.

--- src/main/java/org/skypro/skyshop/model/service/SearchService.java
+++ src/main/java/org/skypro/skyshop/model/service/SearchService.java
@@ -1,5 +1,6 @@
 package org.skypro.skyshop.model.service;
 
+import org.skypro.skyshop.model.search.SearchResult;
 import org.skypro.skyshop.model.search.Searchable;
 import org.springframework.stereotype.Service;
 
@@ -17,7 +18,7 @@ public class SearchService {
     public List<Searchable> search(String query) {
         String queryLower = query.toLowerCase();
         return storageService.getAll().stream()
-                .filter(item -> item.getName().toLowerCase().contains(queryLower))
+                .filter(item -> item.getStringRepresentation().toLowerCase().contains(queryLower))
                 .toList();
     }
 
