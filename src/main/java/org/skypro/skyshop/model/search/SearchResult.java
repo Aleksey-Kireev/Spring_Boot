1f4716f4e295c94e9ecfc2a60bc7bf3e5744a935
Aleksey-Kireev <kireev_a_a@mail.ru>
2025-07-20 04:26:09 +0900

    DZ - 9 - Spring Boot На проверку.

---
+++ src/main/java/org/skypro/skyshop/model/search/SearchResult.java
@@ -0,0 +1,21 @@
+package org.skypro.skyshop.model.search;
+
+import java.util.UUID;
+
+public class SearchResult {
+
+    private final String name;
+    private final UUID id;
+    private final String contentType;
+
+    public SearchResult(UUID id, String name, String contentType) {
+        this.name = name;
+        this.id = id;
+        this.contentType = contentType;
+    }
+
+    public static SearchResult fromSearchable(Searchable searchable) {
+        return new SearchResult(searchable.getId(), searchable.getName(), searchable.getContentType());
+    }
+
+}
